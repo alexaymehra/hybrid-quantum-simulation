ba484963048bb2e665124007ca31e188e2fe63cc
Alexay Mehra <anmehra@ncsu.edu>
2025-09-28 15:20:48 -0400

    complete morse potential & gates v1

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,8 +1,8 @@
 """
 Filename: constants.py
 Author: Alexay Mehra
-Date: 2025-09-10
-Description: Holds constants to be used in operators.
+Date: 2025-09-28
+Description: Class to hold constants and parameters for the morse potential
 """
 
 # Imports
@@ -12,25 +12,20 @@ import numpy as np
 class MorsePotential():
     """
     Create an Instance of the Morse Potential:
-    m = mass
+    mass = reduced mass of diatomic system
     de = well depth
     b = width parameter
-    x0 = equilibrium length of the bond
+    x0 = equilibrium bond length
     hbar = Reduced Planck's Constant (default 1 for easy units)
+    omega = angular frequency of morse potential
     """
-    def __init__(self, mass=1, de=8, b=1, x0=0, hbar=1):
+    def __init__(self, mass=1, de=8, b=1, x0=0, hbar=1, omega=1):
         self.mass = mass
         self.de = de
         self.b = b
         self.x0 = x0
-        self.hbar = hbar
-        self.morse_cap = (int) (np.sqrt(2 * mass * de) / (b * hbar) - (1/2))
-        return
-
-time = 1 # Time to optimize over
-
-# Oscillator Parameters
-omega = 1.0  # Oscillator frequency
-chi = 0.01   # Qubit-oscillator coupling
-
-N = 10
+        self.force_const = 2 * de * (b ** 2)
+        self.angular_freq = b * np.sqrt(2 * de / mass)
+        lamda = np.sqrt(2 * mass * de) / (b * hbar)
+        self.num_bound_states = np.floor(lamda - (1/2)) + 1
+        self.max_q_number = self.num_bound_states - 1
--- src/utils/myops.py
+++ src/utils/myops.py
@@ -1,7 +1,7 @@
 """
 Filename: myops.py
 Author: Alexay Mehra
-Date: 2025-09-10
+Date: 2025-09-28
 
 """
 
@@ -18,15 +18,19 @@ class Gates():
     I_o = np.eye(dim)                                   # Identity operator for qumodes
 
     a = np.diag(np.sqrt(np.arange(1, dim)), 1)          # Annihilation operator
-    adag = a.T.conj()                                   # Creation operator
+    adag = np.transpose(np.conj(a))                     # Creation operator
     n_op = adag @ a                                     # Photon number operator
+
+    x_op = (a + adag)/(np.sqrt(2))                      # Position quadrature operator
+    p_op = -1j * (a - adag)/(np.sqrt(2))                # Momentum quadrature operator
 
     sigma_x = np.array([[0, 1], [1, 0]])                # Pauli X Gate
     sigma_y = np.array([[0, -1j], [1j, 0]])             # Pauli Y Gate
     sigma_z = np.array([[1, 0], [0, -1]])               # Pauli Z Gatez
 
-    def __init__(self, dim=20):
+    def __init__(self, dim=20, time=1):
         self.dim = dim
+        self.time = time
         return
     
     def always_on_evolution(self, omega=1, chi=0.01, t=time):
@@ -36,71 +40,44 @@ class Gates():
         return evolution
     
     def displacement(self, alpha):
-        A = alpha * self.adag - np.conj(alpha) * self.a
-        return sp.linalg.expm(A) 
+        exponent = alpha * self.adag - np.conj(alpha) * self.a
+        gate = sp.linalg.expm(exponent)
+        return gate
     
-    def full_displacement(self):
-        pass
+    def full_displacement(self, alpha):
+        full_gate = np.kron(self.I_q, self.displacement(alpha))
+        return full_gate
 
-    def qubit_xy_rotation(self):
-        pass
+    def qubit_xy_rotation(self, theta, phi):
+        exponent = (np.cos(phi) * self.sigma_x + np.sin(phi) * self.sigma_z)
+        gate = sp.linalg.expm(-1j * (theta * 2) * exponent)
+        return gate
 
-    def full_qubit_xy_rotation(self):
-        pass
+    def full_qubit_xy_rotation(self, theta, phi):
+        full_gate = np.kron(self.qubit_xy_rotation(theta, phi), self.I_o)
+        return full_gate
 
-    def morse_hamiltonian(self):
-        pass
+    def morse_hamiltonian(self, mp):
+        """
+        Create the Morse Hamiltonian from the given parameters
+        """
+        phys_x = np.sqrt(mp.hbar / (mp.mass * mp.angular_freq)) * self.x_op
+        phys_p = np.sqrt(mp.hbar * mp.mass * mp.angular_freq) * self.p_op
+        
+        kinetic_part = (phys_p @ phys_p) * (1 / (2 * mp.mass))
+        exponent_term = -1 * mp.b * (phys_x - mp.x0 * self.I_o)
+        position_part = mp.de * ((1 - sp.linalg.expm(exponent_term)) ** 2)
+        hamiltonian = kinetic_part + position_part
 
-    def morse_time_evolution(self):
-        pass
+        full_kin_part = np.kron(self.I_q, kinetic_part)
+        full_pos_part = np.kron(self.I_q, position_part)
+        full_hamiltonian = np.kron(self.I_q, hamiltonian)
 
-
-
-# Dispacement Gate (Qubit-Qumode Hilbert Space)
-def D_full(alpha):
-    return np.kron(I_q, displacement(alpha))
-
-# Qubit XY Rotation Gate
-def R_phi(theta, phi):
-    op = (np.cos(phi) * sigma_x + np.sin(phi) * sigma_y)
-    return sp.linalg.expm(-1j * theta / 2 * op)
-
-# Qubit XY Rotation Gate (Qubit-Qumode Hilbert Space)
-def R_full(theta, phi):
-    return np.kron(R_phi(theta, phi), I_o)
-
-
-# Morse Hamiltonian (Returns the Matrix from the Qubit-Qumode Hilbert Space)
-def H_Morse(u ,de, b, x0):
-    """
-    Morse Hamiltonian Gate
-
-    Args:
-        u (real): reduced mass of diatomic system
-        de (real): dissociation energy
-        b (real): width parameter
-        x0 (real): equilibrium bond length
-
-    Returns:
-        csc_matrix: operator matrix
-    """
-    m_omega = np.sqrt(2 * de * (b ** 2) / u)
-    X_op = (a + adag) / np.sqrt(2)
-    P_op = 1j * (a - adag) / (np.sqrt(2))
-    x_op = X_op * np.sqrt(hbar / (u * m_omega))
-    p_op = P_op * np.sqrt(hbar * u * omega)
-
-    kin_op = (p_op @ p_op) / (2 * u)
-
-    exp_term = sp.linalg.expm(-1 * b * (x_op - x0 * np.eye(N)))
-    mp_op = de * ((np.eye(N) - exp_term) @ (np.eye(N) - exp_term))
-
-    full_m = kin_op + mp_op
-
-    full_m = np.kron(I_q, full_m)
-
-    return full_m
-
-# Time Evolution for the Morse Hamiltonian
-def MH_Evo(t, u, de, b, x0):
-    return sp.linalg.expm(-1j * H_Morse(u, de, b, x0) * t)
+        return full_hamiltonian, full_pos_part, full_kin_part
+    
+    def morse_time_evolution(self, mp):
+        ham, pos, kin = self.morse_hamiltonian(mp)
+        exp_ham = sp.linalg.expm(-1j* ham * self.time)
+        exp_pos = sp.linalg.expm(-1j* pos * self.time)
+        exp_kin = sp.linalg.expm(-1j* kin * self.time)
+        return exp_ham, exp_pos, exp_kin
